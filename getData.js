cfb614ff3ef2b522f29b9cb177a15f0264835303
TSYSTANOV <tsystanov.vitaly@gmail.com>
2024-03-09 22:46:32 +0200

    add addAndDelete Likes logic

--- getData.js
+++ getData.js
@@ -1,82 +1,96 @@
-import userInformation from "./autorisation.js"
-import { renderPhotos } from "./renderPhotos.js"
+import userInformation from "./autorisation.js";
+import { renderPhotos } from "./renderPhotos.js";
 
-const BASE_URL = 'https://api.unsplash.com/photos'
-const ACCESS_KEY = 'GqqBFzYQGOPScWyBy2Nd2pRcSQr-rvM5ZtB2OEshgTg'
-const SECRET_KEY = 'XCC4EgqV5ot8QOf0TJ4uhsKlQ9KZJo430w3M6I-fP64'
+const BASE_URL = "https://api.unsplash.com/photos";
+const ACCESS_KEY = "GqqBFzYQGOPScWyBy2Nd2pRcSQr-rvM5ZtB2OEshgTg";
+const SECRET_KEY = "XCC4EgqV5ot8QOf0TJ4uhsKlQ9KZJo430w3M6I-fP64";
 
 const head = {
-    Authorization: `Client-ID ${ACCESS_KEY}`,
+  Authorization: `Client-ID ${ACCESS_KEY}`,
+};
+
+async function getAllPhotosAndRender(page = 1) {
+  let data = await fetch(`${BASE_URL}?per_page=30&page=${page}`, {
+    method: "GET",
+    headers: head,
+  }).then((response) => response.json());
+  console.log(data);
+  renderPhotos(data);
 }
 
-
-async function getAllPhotosAndRender(page = 1){
-    let data = await fetch(`${BASE_URL}?per_page=30&page=${page}`,{
-        method:'GET',
-        headers:head,
-    }).then(response=>response.json())
-    console.log(data)
-    renderPhotos(data)
-}
-
-export const getPhoto = async(id)=>{
-    let photoId = new URL(location).searchParams.get('photo')
-    let data = await fetch(`${BASE_URL}/${photoId}`,{
-        method:'GET',
-        headers:head,
-    }).then(response=>response.json())
-    console.log(data)
-    renderPhotos(null, data)
-}
+export const getPhoto = async (id) => {
+  let photoId = new URL(location).searchParams.get("photo");
+  let data = await fetch(`${BASE_URL}/${photoId}`, {
+    method: "GET",
+    headers: head,
+  }).then((response) => response.json());
+  console.log(data);
+  renderPhotos(null, data);
+};
 
 export const authInServer = () => {
-    let urla = new URL('https://unsplash.com/oauth/authorize')
-    urla.searchParams.append('client_id', ACCESS_KEY)
-    urla.searchParams.append('redirect_uri', 'http://127.0.0.1:5500/')
-    urla.searchParams.append('response_type', 'code')
-    urla.searchParams.append('scope', 'public read_user read_photos write_likes')
+  let urla = new URL("https://unsplash.com/oauth/authorize");
+  urla.searchParams.append("client_id", ACCESS_KEY);
+  urla.searchParams.append("redirect_uri", "http://127.0.0.1:5500/");
+  urla.searchParams.append("response_type", "code");
+  urla.searchParams.append("scope", "public read_user read_photos write_likes");
 
-    location.href = urla
-}
-async function checkCode(){
-    let urla = new URL('https://unsplash.com/oauth/token')
-    urla.searchParams.append('client_id', ACCESS_KEY)
-    urla.searchParams.append('client_secret', SECRET_KEY)
-    urla.searchParams.append('redirect_uri', 'http://127.0.0.1:5500/')
-    urla.searchParams.append('grant_type', 'authorization_code')
+  location.href = urla;
+};
+async function checkCode() {
+  let urla = new URL("https://unsplash.com/oauth/token");
+  urla.searchParams.append("client_id", ACCESS_KEY);
+  urla.searchParams.append("client_secret", SECRET_KEY);
+  urla.searchParams.append("redirect_uri", "http://127.0.0.1:5500/");
+  urla.searchParams.append("grant_type", "authorization_code");
 
-    let code = new URL(location).searchParams.get('code')
-    
-    if(code){
-        urla.searchParams.append('code', `${code}`)
-        let data = await fetch(urla,{
-                method:'POST'
-            }).then(respose=>respose.json())
-        console.log(data)
-        localStorage.setItem('UserAccessKey', `Bearer ${data.access_token}`)
-        history.pushState({}, document.title, 'http://127.0.0.1:5500/index.html' )
-        return true
-    }
-    if(localStorage.getItem('UserAccessKey')){
-        return true
-    }
-   
-    return false
-}
+  let code = new URL(location).searchParams.get("code");
 
-export const getUserProfile = async() => {
-    
-    let UserAccessKey = localStorage.getItem('UserAccessKey')
-    let data = await fetch(`https://api.unsplash.com/me`,{
-        method:'GET',
-        headers:{Authorization: UserAccessKey}
-    }).then(response => response.json())
-    userInformation(data)
+  if (code) {
+    urla.searchParams.append("code", `${code}`);
+    let data = await fetch(urla, {
+      method: "POST",
+    }).then((respose) => respose.json());
+    console.log(data);
+    localStorage.setItem("UserAccessKey", `Bearer ${data.access_token}`);
+    history.pushState({}, document.title, "http://127.0.0.1:5500/index.html");
+    return true;
+  }
+  if (localStorage.getItem("UserAccessKey")) {
+    return true;
+  }
 
+  return false;
 }
 
+export const getUserProfile = async () => {
+  let UserAccessKey = localStorage.getItem("UserAccessKey");
+  let data = await fetch(`https://api.unsplash.com/me`, {
+    method: "GET",
+    headers: { Authorization: UserAccessKey },
+  }).then((response) => response.json());
+  userInformation(data);
+};
 
-export default checkCode
-export const getData = (page)=>{
-    getAllPhotosAndRender(page)
-}
+export const addLike = async (id) => {
+  let UserAccessKey = localStorage.getItem("UserAccessKey");
+  let data = await fetch(`${BASE_URL}/${id}/like`, {
+    method: "POST",
+    headers: { Authorization: UserAccessKey },
+  }).then((response) => response.json());
+  return data;
+};
+
+export const deleteLike = async (id) => {
+  let UserAccessKey = localStorage.getItem("UserAccessKey");
+  let data = await fetch(`${BASE_URL}/${id}/like`, {
+    method: "DELETE",
+    headers: { Authorization: UserAccessKey },
+  }).then((response) => response.json());
+  return data;
+};
+
+export default checkCode;
+export const getData = (page) => {
+  getAllPhotosAndRender(page);
+};
--- renderPhotos.js
+++ renderPhotos.js
@@ -1,29 +1,29 @@
-import { loadMore } from "./loadMoreScroll.js"
+import { addLike, deleteLike } from "./getData.js";
+import { loadMore } from "./loadMoreScroll.js";
 
-const ROOT_forAllCards = document.querySelector('.grid')
-const ROOT_forOnePhoto = document.querySelector('.photo__wrapper')
+const ROOT_forAllCards = document.querySelector(".grid");
+const ROOT_forOnePhoto = document.querySelector(".photo__wrapper");
+const btnAuth = document.querySelector(".header__login-button");
 
-function renderImg(src, alt){
-
-    return new Promise((resolve, reject) => {
-        let img = document.createElement('img')
-        img.style.width = '200'
-        img.src = src
-        img.alt = alt
-        img.width = '200'
-        img.addEventListener('load',()=>{
-            resolve(img)
-        })
-    })
-    
+function renderImg(src, alt) {
+  return new Promise((resolve, reject) => {
+    let img = document.createElement("img");
+    img.style.width = "200";
+    img.src = src;
+    img.alt = alt;
+    img.width = "200";
+    img.addEventListener("load", () => {
+      resolve(img);
+    });
+  });
 }
 
-
-async function renderAllPhotos(data){
-    let elems = await Promise.allSettled(data.map(async(el)=>{
-    let li = document.createElement('li')
-    li.className = 'card'
-    li.innerHTML = `
+async function renderAllPhotos(data) {
+  let elems = await Promise.allSettled(
+    data.map(async (el) => {
+      let li = document.createElement("li");
+      li.className = "card";
+      li.innerHTML = `
         <a id="${el.id}" class="grid-item" href="page.html?photo=${el.id}">
             <a class="card__author" href="${el.user.links.html}" target='_blank'>
               <img class="author__photo" src="${el.user.profile_image.small}" role="presentation" alt="${el.user.bio}" title="${el.user.name}">
@@ -31,47 +31,46 @@ async function renderAllPhotos(data){
             <button class="card__photo-like">${el.likes}</button>
             <a class="card__download" href="${el.links.download}" download="" target="_blank"></a>
         </a>
-        `
-        let img = await renderImg(el.urls.small, el.alt_description)
-        li.querySelector('.grid-item').prepend(img)
-        return li
-    // <img width="200" src="${el.urls.small}" alt="${el.alt_description}">
+        `;
+      let img = await renderImg(el.urls.small, el.alt_description);
+      li.querySelector(".grid-item").prepend(img);
+      return li;
     })
-    )
-    elems.forEach((el)=>{
-        if(el.status === 'fulfilled'){
-            ROOT_forAllCards.append(el.value)
-        }else{
-            console.warn(`Some error with loading element: Status:${el.status}, Value:${el.value}`)
-        }
-    })
-    let endElement = document.querySelector('[data-page]')
-    if(endElement){
-        let page = endElement.dataset.page;
-        endElement.remove()
-        let endElem = document.createElement('div')
-        endElem.dataset.page = page
-        ROOT_forAllCards.after(endElem)
-        loadMore(endElem)
-    }else{
-        let endElem = document.createElement('div')
-        endElem.dataset.page = 1
-        ROOT_forAllCards.after(endElem)
-        loadMore(endElem)
+  );
+  elems.forEach((el) => {
+    if (el.status === "fulfilled") {
+      ROOT_forAllCards.append(el.value);
+    } else {
+      console.warn(
+        `Some error with loading element: Status:${el.status}, Value:${el.value}`
+      );
     }
-    
-    const grid = new Masonry(ROOT_forAllCards, {
-        gutter: 10,
-        itemSelector: ".card",
-        columnWidth: 200,
-        isFitWidth: true,
-      });
+  });
+  let endElement = document.querySelector("[data-page]");
+  if (endElement) {
+    let page = endElement.dataset.page;
+    endElement.remove();
+    let endElem = document.createElement("div");
+    endElem.dataset.page = page;
+    ROOT_forAllCards.after(endElem);
+    loadMore(endElem);
+  } else {
+    let endElem = document.createElement("div");
+    endElem.dataset.page = 1;
+    ROOT_forAllCards.after(endElem);
+    loadMore(endElem);
+  }
+
+  const grid = new Masonry(ROOT_forAllCards, {
+    gutter: 10,
+    itemSelector: ".card",
+    columnWidth: 200,
+    isFitWidth: true,
+  });
 }
 
-
-
-function renderOnePhoto(data){
-    ROOT_forOnePhoto.innerHTML = `
+function renderOnePhoto(data) {
+  ROOT_forOnePhoto.innerHTML = `
     <img
             class="photo__picture"
             src="${data.urls.regular}"
@@ -85,7 +84,6 @@ function renderOnePhoto(data){
             /><span>${data.user.name}</span></a
           >
           <div class="photo__control">
-            <button id="${data.id}" class="photo__like">${data.likes}</button>
             <a
               class="photo__download"
               download="true"
@@ -93,13 +91,42 @@ function renderOnePhoto(data){
               target="_blank"
             ></a>
     </div>
-    `
+    `;
+  let btnLikes = document.createElement("button");
+  btnLikes.id = data.id;
+  btnLikes.className = "photo__like";
+  btnLikes.innerHTML = data.likes;
+  if (data.liked_by_user === true) {
+    btnLikes.style.backgroundImage = 'url("../img/like.svg")';
+  } else {
+    btnLikes.style.backgroundImage = 'url("../img/o-like.svg")';
+  }
+  btnLikes.addEventListener("click", async () => {
+    if (btnAuth.dataset.login === "true") {
+      if (data.liked_by_user === true) {
+        btnLikes.style.backgroundImage = 'url("../img/o-like.svg")';
+        let { photo } = await deleteLike(data.id);
+        data.likes = photo.likes;
+        btnLikes.innerHTML = data.likes;
+        data.liked_by_user = photo.liked_by_user;
+      } else {
+        btnLikes.style.backgroundImage = 'url("../img/like.svg")';
+        let { photo } = await addLike(data.id);
+        data.likes = photo.likes;
+        btnLikes.innerHTML = data.likes;
+        data.liked_by_user = photo.liked_by_user;
+      }
+    } else {
+      console.log("Need authorisation");
+    }
+  });
+  ROOT_forOnePhoto.querySelector(".photo__control").prepend(btnLikes);
 }
 
-export const renderPhotos = (array, onePhoto) =>{
-    if(array){
-        renderAllPhotos(array)
-    }else{
-        renderOnePhoto(onePhoto)
-    }
-}
+export const renderPhotos = (array, onePhoto) => {
+  if (array) {
+    renderAllPhotos(array);
+  } else {
+    renderOnePhoto(onePhoto);
+  }
+};
